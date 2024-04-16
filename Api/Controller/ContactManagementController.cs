82e40c48db61e416ddc99a4237998ad665bb0070
Sergei Kamianetskii <i@ksergey.ru>
2024-04-16 23:42:32 +0300

    Рефакторинг контроллера контактов

--- Api/Controller/ContactManagementController.cs
+++ Api/Controller/ContactManagementController.cs
@@ -12,50 +12,25 @@ public class ContactManagementController : BaseController
     [HttpPost("contacts")]
     public void Create([FromBody] Contact contact)
     {
-        storage.Contacts.Add(contact);
+        storage.Add(contact);
     }
 
     [HttpGet("contacts")]
     public List<Contact> GetContacts()
     {
-        return storage.Contacts;
+        return storage.GetContacts();
     }
 
     [HttpDelete("contacts/{id}")]
     public void DeleteContact(int id)
     {
-        Contact contact;
-        for (int i = 0; i < storage.Contacts.Count; i++)
-        {
-            if (storage.Contacts[i].Id == id)
-            {
-                contact = storage.Contacts[i];
-                storage.Contacts.Remove(contact);
-                return;
-            }
-        }
+        storage.Remove(id);
     }
 
     [HttpPut("contacts/{id}")]
     public void UpdateContact([FromBody] ContactDto contactDto, int id)
     {
-        Contact contact;
-        for (int i = 0; i < storage.Contacts.Count; i++)
-        {
-            if (storage.Contacts[i].Id == id)
-            {
-                contact = storage.Contacts[i];
-                if (!String.IsNullOrEmpty(contactDto.Email))
-                {
-                    contact.Email = contactDto.Email;
-                }
-                if (!String.IsNullOrEmpty(contactDto.Name))
-                {
-                    contact.Name = contactDto.Name;
-                }
-                return;
-            }
-        }
+        storage.UpdateContact(contactDto, id);
     }
 
 }
--- Api/Storage/ContactStorage.cs
+++ Api/Storage/ContactStorage.cs
@@ -1,5 +1,7 @@
 public class ContactStorage
 {
+    private List<Contact> Contacts { get; set; }
+
     public ContactStorage()
     {
         this.Contacts = new List<Contact>();
@@ -15,5 +17,50 @@ public class ContactStorage
         }
     }
 
-    public List<Contact> Contacts { get; set; }
+
+
+    public List<Contact> GetContacts()
+    {
+        return Contacts;
+    }
+
+    public void Add(Contact contact)
+    {
+        Contacts.Add(contact);
+    }
+
+    public void Remove(int id)
+    {
+        Contact contact;
+        for (int i = 0; i < Contacts.Count; i++)
+        {
+            if (this.Contacts[i].Id == id)
+            {
+                contact = Contacts[i];
+                Contacts.Remove(contact);
+                return;
+            }
+        }
+    }
+
+    public void UpdateContact(ContactDto contactDto, int id)
+    {
+        Contact contact;
+        for (int i = 0; i < Contacts.Count; i++)
+        {
+            if (Contacts[i].Id == id)
+            {
+                contact = Contacts[i];
+                if (!String.IsNullOrEmpty(contactDto.Email))
+                {
+                    contact.Email = contactDto.Email;
+                }
+                if (!String.IsNullOrEmpty(contactDto.Name))
+                {
+                    contact.Name = contactDto.Name;
+                }
+                return;
+            }
+        }
+    }
 }
